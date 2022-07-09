5eba30d93be00bad9c1a63bd80f78326050728d1
him-a-n-shu <108982340+him-a-n-shu@users.noreply.github.com>
2022-07-09 17:22:37 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,27 @@
+#Calculator
+
+val1 = float(input("Enter value 1: "))
+val2 = float(input("Enter value 2: "))
+op = input("Enter any one of the operator (+,-,*,/): ")
+
+if op == "+":
+    result = val1 + val2
+
+elif op == "-":
+    if val1 > val2:
+        result = val1 - val2
+    else:
+        result = val2 - val1
+
+elif op == "*":
+    result = val1 * val2
+
+elif op == "/":
+    if val2 == 0:
+        print("Error! Division by zero is not allowed. Program terminated")
+    else:
+        result = val1/val2
+
+else:
+    print("Wrong input,program terminated")
+    print("The result is ",result)
